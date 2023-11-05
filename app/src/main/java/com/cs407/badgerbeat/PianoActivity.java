84810e09ca3a2aec695fd33cc12a3d56fa991ae6
IsaacKohl <ikohl@wisc.edu>
2023-11-05 16:04:01 -0600

    Added piano note sounds fr this time and added piano picture and Back button

--- app/src/main/java/com/cs407/badgerbeat/PianoActivity.java
+++ app/src/main/java/com/cs407/badgerbeat/PianoActivity.java
@@ -2,13 +2,34 @@ package com.cs407.badgerbeat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PianoActivity extends AppCompatActivity {
-
+    private Button backButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_piano);
+
+        //Sets buttons
+        backButton = findViewById(R.id.backButton);
+        //Sets Media PLayer
+        final MediaPlayer mp = MediaPlayer.create(this, R.raw.key01);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mp.start(); //Plays song
+                toHome();
+            }
+        });
+    }
+
+    private void toHome() { //Returns to home page
+        Intent intent = new Intent(this, HomeActivity.class);
+        startActivity(intent);
     }
 }
