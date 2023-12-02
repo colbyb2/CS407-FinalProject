540b9e0be1c916c963dafd329b1fff74ae3b9271
IsaacKohl <ikohl@wisc.edu>
2023-12-02 17:36:41 -0600

    Have one key working, a few other small changes

--- app/src/main/java/com/cs407/badgerbeat/PianoActivity.java
+++ app/src/main/java/com/cs407/badgerbeat/PianoActivity.java
@@ -7,9 +7,11 @@ import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import java.util.Hashtable;
 
 public class PianoActivity extends AppCompatActivity {
     private Button backButton;
+    private Button c2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,12 +19,24 @@ public class PianoActivity extends AppCompatActivity {
 
         //Sets buttons
         backButton = findViewById(R.id.backButton);
+        c2 = findViewById(R.id.c2);
         //Sets Media PLayer
-        final MediaPlayer mp = MediaPlayer.create(this, R.raw.f2);
+        final MediaPlayer mp = MediaPlayer.create(this, R.raw.key08c); //
+        c2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mp.isPlaying()) {
+                    mp.stop();
+
+                    mp.start();
+                }
+                mp.start(); //Plays note
+            }
+        });
+
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mp.start(); //Plays song
                 toHome();
             }
         });
